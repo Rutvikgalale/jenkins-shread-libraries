cdb6a86a2e11acedee0665ba5cdff676a9ff0dcb
Rutvikgalale <rutvikgalale16@gmail.com>
2025-11-29 21:57:10 +0530

    Create docker_push.groovy

---
+++ vars/docker_push.groovy
@@ -0,0 +1,11 @@
+def call(String image_name,String image_tag, String DockerHubUser){
+  echo "pushing image to docker Hub"
+  withCredentials([usernamePassword(credentialsId: "DockerHubcred", passwordVariable: "DockerHubPass", usernameVariable: "DockerHubUser")]) {
+    sh '''
+    echo "USER=$DockerHubUser"
+    echo "PASS length=${#DockerHubPass}"
+    docker login -u $DockerHubUser -p $DockerHubPass
+    docker tag ${image_name}:${image_tag} $DockerHubUser/${image_name}:${image_tag}
+    docker push $DockerHubUser/${image_name}:${image_tag}
+    '''
+  }
