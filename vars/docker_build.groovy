ba6299d17d543c10529e76f50bce805b99dcf45e
Rutvikgalale <rutvikgalale16@gmail.com>
2025-11-30 00:04:53 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,5 +1,5 @@
 def call(String image_name, String image_tag, String DockerHubUser){
-  echo "This is building code
+  echo "This is building code"
   sh "docker build -t ${DockerHubUser}/${image_name}:${image_tag} . "
   echo "docker build successfull"
 }
