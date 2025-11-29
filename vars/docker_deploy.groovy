a6b781a23c87ee7f6cf68568582149338064fe51
Rutvikgalale <rutvikgalale16@gmail.com>
2025-11-30 00:22:25 +0530

    Update docker_deploy.groovy

--- vars/docker_deploy.groovy
+++ vars/docker_deploy.groovy
@@ -5,11 +5,11 @@ def call(String image_name, String image_tag, String port){
   #docker stop notes-app || true
   #docker rm notes-app || true
   # Stop any container occupying port 8000
-  old=$(docker ps -aq --filter "publish=$port")
-  if [ -n "$old" ]; then
+  old=\$(docker ps -aq --filter "publish=$port")
+  if [ -n "\$old" ]; then
   echo "Stopping containers using port $port..."
-  docker stop $old || true
-  docker rm $old || true
+  docker stop \$old || true
+  docker rm \$old || true
   fi
   /usr/bin/docker run -dit -p ${port}:${port} ${image_name}:${image_tag}
   """
