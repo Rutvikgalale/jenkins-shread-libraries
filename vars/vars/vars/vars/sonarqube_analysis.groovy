b6b290c4ea0244b394d5773ca5c2bec5aa250a2a
Rutvikgalale <rutvikgalale16@gmail.com>
2025-12-06 11:56:55 +0530

    Delete vars/vars directory

--- vars/vars/vars/vars/sonarqube_analysis.groovy
+++
@@ -1,4 +0,0 @@
-def call(String SonarQubeserver, String Projectname, String ProjectKey){
-  withSonarQubeEnv("${SonarQubeserver}"){
-    sh "$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=${Projectname} -Dsonar.projectKey=${ProjectKey} -X"
-}
